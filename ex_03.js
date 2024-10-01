537b51545cc502cb11e33925b1d490ae8dcb5177
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-10-01 11:57:15 +0200

    changement backround color

--- ex_03.js
+++ ex_03.js
@@ -1,16 +1,20 @@
 function changebackground() {
-    var magicNumber = 1;
-    let magicNumber = 2; //erreur dans le code icu :magic number déjà declaré,fait exprès?
-    let magicNumber = 3;
   
+  
+  
+    {var magicNumber = 1;
     if (magicNumber === 1) {
-      paintRedRectangle();
-    }
+      paintYellowRectangle();}
+    
+    {let magicNumber = 2;
     if (magicNumber === 2) {
-      paintGreenRectangle();
-    }
-    if (magicNumber === 3) {
-      paintYellowRectangle();
+      paintGreenRectangle()}}
+      
+    
+    {let magicNumber = 3;
+    if (magicNumber === 3) 
+      paintRedRectangle()}
+      
     }
   }
   
