177871dce287aa98c0f6eba64c0413a7b893d221
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-10-01 17:40:56 +0200

    calcul du 9

--- ex_09bis.js
+++ ex_09bis.js
@@ -1,8 +1,8 @@
 const div = []
 function operation(a,b,cal){
 		let vre=a + cal + b
-    vre.toString()
-    div.push(vre)
+      vre.toString()
+     div.push(vre)
     
 }
 operation(23,98,"-")
@@ -16,36 +16,36 @@ function operationa(a, b, cal) {
     switch (cal) {
         case "+":
             result = a + b;
-            break;
-        case "-":
+             break;
+          case "-":
             result = a - b;
             break;
-        case "*":
+         case "*":
             result = a * b;
             break;
-        case "/":
-            if (b !== 0) {
+      case "/":
+             if (b !== 0) {
                 result = a / b;
-            } else {
+            }else {
                 console.log("Ne peux pas diviser par 0");
-                return null; 
+                   return null; 
             }
             break;
-        default:
-            console.log("Erreur,renvoie null");
-            return null; 
+    default:
+          console.log("Erreur,renvoie null");
+                 return null; 
     }
 
     diva.push(result.toString()); 
-    return result; 
-}
+           return result; 
+} 
 
 function doOperation(operationString) {
    
-    const parts = operationString.split(" ");
+         const parts = operationString.split(" ");
     const a = parseFloat(parts[0]); 
-    const operator = parts[1]; 
-    const b = parseFloat(parts[2]); 
+     const operator = parts[1]; 
+      const b = parseFloat(parts[2]); 
 
     
     return operationa(a, b, operator);
